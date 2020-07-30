c6007f3eb3bd3417cff6d61ddb4b373c58c8f10e
Nicholas Leask <leask.nicholas@gmail.com>
2020-07-30 14:57:20 -0400

    initial commit

---
+++ queries.js
@@ -0,0 +1,17 @@
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user: 'postgres', 
+    host: 'portfolio-instance.cnuypb0q01wm.us-east-1.rds.amazonaws.com', 
+    database: 'projects', 
+    password: 'password',
+    port: '5432',
+})
+const getProjects = (request, response) => {
+    pool.query('SELECT * FROM projects', (error, results) => {
+        if(error) {
+            throw error;
+        }
+        response.status(200).json(results.rows);
+    })
+}
+module.exports = { getProjects }
